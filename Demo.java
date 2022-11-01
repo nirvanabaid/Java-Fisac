8d6c804f4f4557d1e13a6d49fdbeb4fe6cdb6f9e
nirvanabaid <baidnirvana@gmail.com>
2022-11-01 15:10:26 +0530

    nirbvana

--- Demo.java
+++ Demo.java
@@ -1,7 +1,34 @@
 import java.util.*;
+
+
+abstract class Info
+{
+    static int a[]= new int [10];
+    static int s=0;
+    static HashMap<String,String> employeeMap=new HashMap<String,String>();
+    static HashMap<String,String> employerMap=new HashMap<String,String>();
+ 
+}
+class Admin extends Info
+{
+    String pass= "123456";
+    void addEmployee(String n, String p)
+    {
+        employeeMap.put(n, p);
+    }
+    void disp(String n)
+    {
+        System.out.println(employeeMap.get(n));
+    }
+}
 class Demo
 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Admin a= new Admin();
+        a.addEmployee("nirvana", "123456");
+        a.addEmployee("aman", "hello");
+        a.disp("aman");
+        a.disp("nirvana");
+        
     }
 }
