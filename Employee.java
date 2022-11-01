d65b31f7388c31b0cf2c17b626a2329f752f68f3
aman03trave <115354994+aman03trave@users.noreply.github.com>
2022-11-01 17:22:24 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,9 +1,25 @@
 import java.util.*;
 class Employee extends Info {
     Scanner in = new Scanner(System.in);
-   void Requestleave(String user_name,String password, String reason, String Status)
+   void Requestleave(String user_name,String status, String reason)
    {
-    
+     leave[s][0] = user_name;
+     leave[s][1] = status;
+     leave[s][2] = reason;
+     s++;
+   }
+   void ReviewLeave(String user_name)
+   {
+    for(int i = 0; i < s; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(leave[i][0] == user_name)
+            {
+                System.out.println(leave[i][j]);
+            }
+        }
+    }
    }
 
 
