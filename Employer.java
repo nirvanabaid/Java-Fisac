601c7339ec01d540eb18c95e5ce8a287309f2945
LoneStanding <92737430+LoneStanding@users.noreply.github.com>
2022-11-01 17:07:31 +0530

    Update Employer.java

--- Employer.java
+++ Employer.java
@@ -1,7 +1,40 @@
+import java.util.*;
+
 public class Employer extends Info{
-    String s= "YAYYY WE DID IT!!!!";
-    void addEmployee(String n, String p)
+    Scanner sc=new Scanner(System.in);
+    void disp_all()
     {
-        employeeMap.put(n, p);
+        for (Map.Entry m: employeeMap.entrySet())
+        {
+            System.out.println("Employee: "+m.getKey());
+        }
+    }
+    void addAttendance(){
+        int stat;
+        System.out.print("Give attendance to all employees\n1 for Present\n0 for absent");
+        for (Map.Entry m: employeeMap.entrySet())
+        {
+            System.out.print("Employee: "+m.getKey()+"Status: ") ;
+            stat=sc.nextInt();
+            if (stat==1){
+                //Give Attendance
+            }
+            else{
+                //No Attendance
+            }
+        }
+    }
+    boolean leaveRec(String n){
+        int approve;
+        System.out.println("0 for Rejection and 1 for Approve");
+        System.out.println("Leave Request");//From Reason of *n* Person
+        approve=sc.nextInt();
+        if (approve==1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
+
